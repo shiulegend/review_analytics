e45e7e486ed802354bd561bc647e6e3abb101548
shiulegend <shiulegend@gmail.com>
2023-04-17 19:20:08 +0800

    read average

--- read.py
+++ read.py
@@ -13,6 +13,12 @@ with open('reviews.txt' , 'r') as f:
         count += 1 
         if count % 1000 == 0 :
             print(len(data))
-print (data[0])      
-print ('-----------------------------')  
-print (data[0])  
+#print (data[0])      
+#print ('-----------------------------')  
+#print (data[0])  
+
+#算平均留言長度
+sum_len = 0 
+for d in data:
+        sum_len  += len(d)
+print('平均長度' , sum_len/len(data))
